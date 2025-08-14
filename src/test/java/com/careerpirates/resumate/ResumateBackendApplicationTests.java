628f55ee902edcecba3e9f04a7648c1a3e6fcd0a
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-08-14 15:42:55 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/com/careerpirates/resumate/ResumateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerpirates.resumate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
