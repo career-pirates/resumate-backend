f275cdbfd4a02d265aaabf2698e311a3670bac2b
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-09-02 17:29:41 +0900

    feat: OncePerRequestFilter 구현체 구현 (#2)
    JWT를 통해 인증 여부를 검증하는 OncePerRequestFilter 구현체 구현

---
+++ src/main/java/com/careerpirates/resumate/auth/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,83 @@
+package com.careerpirates.resumate.auth.security.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.careerpirates.resumate.auth.application.dto.CustomMemberDetails;
+import com.careerpirates.resumate.auth.util.jwt.JwtExtractor;
+import com.careerpirates.resumate.auth.util.jwt.JwtValidator;
+import com.careerpirates.resumate.member.domain.entity.Member;
+import com.careerpirates.resumate.member.infrastructure.MemberRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	private final JwtExtractor extractor;
+	private final JwtValidator validator;
+	private final MemberRepository memberRepository;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+			FilterChain filterChain) throws ServletException, IOException {
+		String token = extractor.resolveToken(request);
+
+		String validResult = validator.validateToken(token);
+		if (!validResult.equals("OK")) {
+			sendUnauthorizedResponse(response, validResult);
+			return;
+		}
+
+		String subject = extractor.parseClaims(token).getSubject();
+
+		if (!StringUtils.hasText(subject)) {
+			sendUnauthorizedResponse(response, "유효하지 않은 토큰입니다.");
+			return;
+		}
+
+		long memberId = Long.parseLong(subject);
+
+		Member member = memberRepository.findById(memberId).orElse(null);
+
+		if (member != null) {
+			CustomMemberDetails userDetails = new CustomMemberDetails(member);
+			UsernamePasswordAuthenticationToken authentication =
+					new UsernamePasswordAuthenticationToken(userDetails, null,
+							userDetails.getAuthorities());
+
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		} else {
+			sendUnauthorizedResponse(response, "유효하지 않은 사용자입니다.");
+			return;
+		}
+
+		filterChain.doFilter(request, response);
+	}
+
+	private void sendUnauthorizedResponse(HttpServletResponse response, String message)
+			throws IOException {
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		response.setContentType("application/json;charset=UTF-8");
+
+		String jsonResponse = String.format("""
+				{
+				  "status": 401,
+				  "message": "%s",
+				  "result": null
+				}
+				""", message);
+
+		response.getWriter().write(jsonResponse);
+	}
+}
