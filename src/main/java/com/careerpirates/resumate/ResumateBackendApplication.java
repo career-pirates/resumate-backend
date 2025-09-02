f0ba9eafcfba05b75d76db2a8a5a0c263ad8aca4
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-09-02 16:18:28 +0900

    chore: @EnableJpaAuditing, @ConfigurationPropertiesScan 어노테이션 추가 (#2)

--- src/main/java/com/careerpirates/resumate/ResumateBackendApplication.java
+++ src/main/java/com/careerpirates/resumate/ResumateBackendApplication.java
@@ -2,8 +2,12 @@ package com.careerpirates.resumate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@ConfigurationPropertiesScan
 public class ResumateBackendApplication {
 
 	public static void main(String[] args) {
