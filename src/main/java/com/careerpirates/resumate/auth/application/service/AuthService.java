a543014d58b18b9d6165fc440efeef781c275465
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-09-02 17:07:40 +0900

    feat: DefaultOAuth2UserService 상속 클래스 구현 (#2)
    각각의 OAuth2 제공자에 대응하기 위해 DefaultOAuth2UserService를 상속하는 커스텀된 서비스 레이어 구현

---
+++ src/main/java/com/careerpirates/resumate/auth/application/service/AuthService.java
@@ -0,0 +1,96 @@
+package com.careerpirates.resumate.auth.application.service;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import com.careerpirates.resumate.auth.application.dto.response.CurrentUserDto;
+import com.careerpirates.resumate.auth.message.exception.OAuthErrorCode;
+import com.careerpirates.resumate.global.message.exception.core.BusinessException;
+import com.careerpirates.resumate.member.domain.entity.Member;
+import com.careerpirates.resumate.member.domain.enums.OAuthProvider;
+import com.careerpirates.resumate.member.domain.enums.Role;
+import com.careerpirates.resumate.member.infrastructure.MemberRepository;
+import com.careerpirates.resumate.member.message.exception.MemberErrorCode;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService extends DefaultOAuth2UserService {
+
+	private final MemberRepository memberRepository;
+
+	@Override
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+		String registrationId = userRequest.getClientRegistration().getRegistrationId();
+		OAuth2User oAuth2User = super.loadUser(userRequest);
+
+		if ("google".equals(registrationId)) {
+			return createOAuth2User(getUserInfoFromGoogle(oAuth2User));
+		} else if ("naver".equals(registrationId)) {
+			return createOAuth2User(getUserInfoFromNaver(oAuth2User));
+		} else if ("kakao".equals(registrationId)) {
+			return createOAuth2User(getUserInfoFromKakao(oAuth2User));
+		}
+
+		throw new BusinessException(OAuthErrorCode.UNSUPPORTED_OAUTH_PROVIDER);
+	}
+
+	public CurrentUserDto getCurrentUser(Long id) {
+		Member member = memberRepository.findById(id)
+			.orElseThrow(() -> new BusinessException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+		return CurrentUserDto.of(member);
+	}
+
+	private Map<String, Object> getUserInfoFromGoogle(OAuth2User oAuth2User) {
+		return new HashMap<>(){{
+			put("id", oAuth2User.getAttribute("sub"));
+			put("provider", OAuthProvider.GOOGLE);
+			put("email", oAuth2User.getAttribute("email"));
+		}};
+	}
+
+	private Map<String, Object> getUserInfoFromNaver(OAuth2User oAuth2User) {
+		Map<String, Object> naverAccount = oAuth2User.getAttribute("response");
+		if (naverAccount == null) {
+			throw new BusinessException(OAuthErrorCode.NAVER_OAUTH2_DATA_MISSING);
+		}
+
+		return new HashMap<>(){{
+			put("id", naverAccount.get("id"));
+			put("provider", OAuthProvider.NAVER);
+			put("email", naverAccount.get("email"));
+		}};
+	}
+
+	private Map<String, Object> getUserInfoFromKakao(OAuth2User oAuth2User) {
+		Map<String, Object> kakaoAccount = oAuth2User.getAttribute("kakao_account");
+		if (kakaoAccount == null) {
+			throw new BusinessException(OAuthErrorCode.KAKAO_OAUTH2_DATA_MISSING);
+		}
+
+		return new HashMap<>(){{
+			put("id", oAuth2User.getAttribute("id"));
+			put("provider", OAuthProvider.KAKAO);
+			put("email", kakaoAccount.get("email"));
+		}};
+	}
+
+	private OAuth2User createOAuth2User(Map<String, Object> attributes) {
+		return new DefaultOAuth2User(
+			Collections.singleton(new SimpleGrantedAuthority(Role.USER.name())),
+			attributes,
+			"email"
+		);
+	}
+}
