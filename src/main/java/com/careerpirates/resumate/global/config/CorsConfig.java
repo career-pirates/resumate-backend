a24f36150ed6de40e7796781a932baae062c23b3
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-09-02 16:30:45 +0900

    refactor: CorsConfig 수정 (#2)
    @Value 어노테이션을 이용하여 환경변수 파일에서 값을 직접 읽어들이는 대신 CorsProperties를 활용하도록 코드 수정

--- src/main/java/com/careerpirates/resumate/global/config/CorsConfig.java
+++ src/main/java/com/careerpirates/resumate/global/config/CorsConfig.java
@@ -1,34 +1,27 @@
 package com.careerpirates.resumate.global.config;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class CorsConfig {
 
-	@Value("#{'${cors.allowed-origins}'.split(',')}")
-	private List<String> allowedOrigins;
-
-	@Value("#{'${cors.allowed-methods}'.split(',')}")
-	private List<String> allowedMethods;
-
-	@Value("#{'${cors.allowed-headers}'.split(',')}")
-	private List<String> allowedHeaders;
+	private final CorsProperties corsProperties;
 
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
 
 		config.setAllowCredentials(true);
-		config.setAllowedOrigins(allowedOrigins);
-		config.setAllowedMethods(allowedMethods);
-		config.setAllowedHeaders(allowedHeaders);
+		config.setAllowedOriginPatterns(corsProperties.allowedOriginPatterns());
+		config.setAllowedMethods(corsProperties.allowedOriginMethods());
+		config.setAllowedHeaders(corsProperties.allowedOriginHeaders());
 
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", config);
