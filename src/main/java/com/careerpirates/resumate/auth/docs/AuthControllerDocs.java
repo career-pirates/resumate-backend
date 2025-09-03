f7fa969aa84afa1aab990c5a184cf4711763476a
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-09-03 11:34:18 +0900

    refactor: @SecurityRequirement 어노테이션 레벨 수정 (#2)

--- src/main/java/com/careerpirates/resumate/auth/docs/AuthControllerDocs.java
+++ src/main/java/com/careerpirates/resumate/auth/docs/AuthControllerDocs.java
@@ -14,16 +14,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Tag(name = "인증", description = "인증 API")
-@SecurityRequirement(name = "cookieAuth")
 public interface AuthControllerDocs {
 
 	@Operation(summary = "로그아웃", description = "AccessToken 및 RefreshToken 쿠키를 삭제하여 로그아웃합니다.")
+	@SecurityRequirement(name = "cookieAuth")
 	SuccessResponse<?> logout(
 		@AuthenticationPrincipal CustomMemberDetails member,
 		@RequestParam String deviceId, HttpServletResponse response
 	);
 
 	@Operation(summary = "액세스 토큰 재발급", description = "리프레시 토큰을 이용해 액세스 토큰을 재발급합니다.")
+	@SecurityRequirement(name = "refreshCookieAuth")
 	SuccessResponse<?> reissueAccessToken(
 		@RequestParam String deviceId,
 		HttpServletRequest request,
@@ -31,6 +32,7 @@ public interface AuthControllerDocs {
 	);
 
 	@Operation(summary = "본인확인", description = "현재 로그인한 사용자의 정보를 조회합니다.")
+	@SecurityRequirement(name = "cookieAuth")
 	SuccessResponse<CurrentUserDto> getCurrentUser(
 		@AuthenticationPrincipal CustomMemberDetails member
 	);
