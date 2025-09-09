47793e28d3ba6c8e877000f9b27edc157fef50dd
Altair5869 <th0538@naver.com>
2025-09-09 09:08:25 +0900

    env: application-test, pr-tests.yml 추가

--- src/test/java/com/careerpirates/resumate/ResumateBackendApplicationTests.java
+++ src/test/java/com/careerpirates/resumate/ResumateBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.careerpirates.resumate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ResumateBackendApplicationTests {
 
 	@Test
