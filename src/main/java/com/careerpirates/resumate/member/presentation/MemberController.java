161fd20a4065c19a837e1cc43819bc242709c0a6
Kim Yeong Ryeol <freekyr7529@gmail.com>
2025-09-09 10:48:26 +0900

    fix: 회원 컨트롤러 버그 수정 (#69)

--- src/main/java/com/careerpirates/resumate/member/presentation/MemberController.java
+++ src/main/java/com/careerpirates/resumate/member/presentation/MemberController.java
@@ -2,6 +2,7 @@ package com.careerpirates.resumate.member.presentation;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.careerpirates.resumate.auth.application.dto.CustomMemberDetails;
@@ -13,7 +14,8 @@ import com.careerpirates.resumate.member.message.success.MemberSuccessCode;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController("/api/member")
+@RestController
+@RequestMapping("/api/member")
 @RequiredArgsConstructor
 public class MemberController implements MemberControllerDocs {
 
